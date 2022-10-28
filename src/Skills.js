43eb6fbfe6fc599eb483a94fa5334f5d3b56a68b
Jaxknz <74647042+Jaxknz@users.noreply.github.com>
2022-10-28 18:53:49 +1300

    Skills page added

---
+++ src/Skills.js
@@ -0,0 +1,15 @@
+const Skills =() =>{
+    return(
+        <div className="Skills" id="skills">
+        <Flex className="section">
+        <Box className="box">
+        <Text className="title">Skills</Text>
+        </Box>
+        </Flex>
+
+        </div>
+
+    );
+}
+
+export default Skills;
