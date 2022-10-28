1f6b652e845d9b6dca94595548e80d9a59f1ae81
Jaxknz <74647042+Jaxknz@users.noreply.github.com>
2022-10-28 22:35:17 +1300

    reverted navbar changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,14 +2,7 @@ import React from 'react';
 import {Flex,ChakraProvider,} from '@chakra-ui/react';
 import {Link} from 'react-scroll';
 import './App.css';
-{/*
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarlinks = document.getElementsByClassName('pageLinks')[0];
 
-toggleButton.addEventListener('click', () =>{
-    navbarlinks.classList.toggle('active')
-});
-*/}
 
 const NavBar =() =>{
  
@@ -19,11 +12,6 @@ const NavBar =() =>{
         <Flex justify="center" width="100%"align="center"   justifyContent="center"  paddingTop="1rem">
             {/*Left side*/}
             <Flex  className="navbox" justify= "center" align="center"   >
-            <a  class="toggle-button">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </a>
             <div className='pageLinks'>
             <ul>
             <li><Link  className='link' to="About" spy={true} smooth={true} offset={-10} duration={500}margin="0 5px">About</Link></li>
