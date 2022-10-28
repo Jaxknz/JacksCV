c3267188ff2634116368ca4a1a356d09644856c3
Jaxknz <74647042+Jaxknz@users.noreply.github.com>
2022-10-28 19:15:57 +1300

    Technical skills added

--- src/Skills.js
+++ src/Skills.js
@@ -9,6 +9,18 @@ const Skills =() =>{
         <Flex className='sectionContent'>
         <Box className='skillsContent'>
         <Text className='jobTitle'>Technical Skills</Text>
+        <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>PHP</li>
+        <li>Javascript</li>
+        <li>Java</li>
+        <li>SQL</li>
+        <li>C#</li>
+        <li>C++</li>
+        <li>Photoshop</li>
+        <li>Microsoft 365 (Excel, word, teams, etc..)</li>
+        </ul>
         </Box>
         <Box className='skillsContent'>
         <Text className='jobTitle'>General Skills</Text>
