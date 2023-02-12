2d72e0a2c2d128ec52ce256f0c113b4d4ff5c86c
Jaxknz <74647042+Jaxknz@users.noreply.github.com>
2023-02-12 19:17:28 +1300

    Test

--- src/Work.js
+++ src/Work.js
@@ -9,6 +9,21 @@ const Work = () => {
                 <Flex className="sectionContent" >
                 
                   <ul className='workStuff'>
+                    {/* Treasury details*/}
+                    <Box className='jobSections'>
+                <Text  className='jobTitle'>Intralot NZ - Application Support
+                <Text className='jobLength'>April 2021– Current</Text>
+                <Text className="jobDetails">I currently hold the role of Application Support at Intralot NZ.  My primary tasks are:</Text>
+                <Text className="jobDescription">
+                <li>Taking phone calls from clients and service technicians.</li>
+                <li>Liaising with off-site contractors to organize equipment replacement.</li>
+                <li>Running daily, weekly, and monthly reports and ensuring they are correct.</li>
+                <li>Monitoring equipment to ensure it is always operational.</li>
+                <li>Testing new software in a test lab to ensure it is compliant.</li>
+                <li>Ensuring all equipment in the test lab is functional.</li>
+                </Text>
+                </Text>
+                </Box>
                                  {/* Intralot details*/}
                                  <Box className='jobSections'>
                 <Text  className='jobTitle'>Intralot NZ - Application Support
